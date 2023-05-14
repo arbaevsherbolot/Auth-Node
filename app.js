21acd92f683e4074d7554d1bf15c21e5a734335f
Sherbolot <arbaevsherbolot1@gmail.com>
2023-05-14 23:53:40 +0600

    UPDATE GLOBAL

--- app.js
+++ app.js
@@ -3,13 +3,27 @@ require("dotenv").config();
 const express = require("express");
 const app = express();
 const cors = require("cors");
+const cookieParser = require("cookie-parser");
+const session = require("express-session");
 
 const route = require("./routes/Route");
 
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
-app.use(cors({ origin: true }));
+app.use(
+  cors({ origin: true, methods: ["GET", "POST", "PUT"], credentials: true })
+);
+app.use(cookieParser());
+app.use(
+  session({
+    key: "userId",
+    secret: "secret",
+    resave: false,
+    saveUninitialized: false,
+    cookie: { expires: 60 * 60 * 24 },
+  })
+);
 
 app.use("/auth", route);
 
--- controllers/Controller.js
+++ controllers/Controller.js
@@ -1,4 +1,7 @@
+require("dotenv").config();
+
 const db = require("../connections/db");
+const jwt = require("jsonwebtoken");
 const bcrypt = require("bcrypt");
 
 const controller = {
@@ -32,15 +35,31 @@ const controller = {
         })
         .then((user) => {
           if (user.length === 0) {
-            res.status(401).json({ message: "User is doesn't exist!" });
+            res
+              .status(401)
+              .json({ auth: false, message: "User is doesn't exist!" });
           } else {
             bcrypt.compare(password, user[0].password, (err, result) => {
               if (result === false) {
-                res
-                  .status(401)
-                  .json({ error: err, message: "Incorrect password!" });
+                res.status(401).json({
+                  auth: false,
+                  message: "Incorrect password!",
+                });
               } else {
+                const userId = user[0].id;
+                const token = jwt.sign(
+                  { userId: userId },
+                  process.env.JWT_ACCESS_SECRET_KEY,
+                  {
+                    expiresIn: "1h",
+                  }
+                );
+
+                req.session.user = user;
+
                 res.status(201).json({
+                  token: token,
+                  auth: true,
                   data: user,
                   message: "User is successfully logged in!",
                 });
@@ -49,9 +68,43 @@ const controller = {
           }
         });
     } catch {
-      res.status(401).json({ message: "Login ERROR!" });
+      res.status(401).json({ auth: false, message: "Login ERROR!" });
     }
   },
+
+  profileUser: async (req, res) => {
+    try {
+      res.status(201).json({ message: "User Profile" });
+    } catch {
+      res.status(401).json({ message: "No DATA!" });
+    }
+  },
+
+  // updateUsernameById: async (req, res) => {
+  //   try {
+  //     const { userId } = req.params;
+  //     const { username } = req.body;
+
+  //     await db("all_users")
+  //       .where({
+  //         id: userId,
+  //       })
+  //       .update({
+  //         username: username,
+  //       });
+
+  //     const user = await db("all_users").where({
+  //       id: userId,
+  //     });
+
+  //     res.status(201).json({
+  //       data: user,
+  //       message: "Username successfully updated!",
+  //     });
+  //   } catch {
+  //     res.status(201).json({ message: "Unable to change Username!" });
+  //   }
+  // },
 };
 
 module.exports = controller;
